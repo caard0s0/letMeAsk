0b9f2d72045ea212f7025497f1e6abe265887b5b
Vinicius <vinicardoso216@gmail.com>
2022-06-13 23:08:21 -0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,107 @@
+import { FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom'
+
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
+import { database } from '../services/firebase';
+
+import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
+
+import '../styles/auth.scss';
+
+export function Home() {
+    const navigate = useNavigate();
+    const { user, signInWithGoogle } = useAuth()
+    const [roomCode, setRoomCode] = useState('');
+    
+    async function handleCreateRoom() {
+        if (!user) {
+            await signInWithGoogle()
+        }
+
+        navigate('/rooms/new');
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()) {
+            alert('Room does not exists.');
+            return;
+        }
+
+        if (roomRef.val().endedAt) { //closedAt 
+            alert('Room already closed.');
+            return;
+        }
+
+        navigate(`/rooms/${roomCode}`);
+    }
+
+    return (
+
+        <div id="page-auth">
+
+            <aside>
+
+                <img src={illustrationImg} alt="Illustration symbolizing questions and answers" />
+
+                <strong>
+                    Create live Q&amp;A rooms
+                </strong>
+
+                <p>
+                    Clear your audience's doubts in real time
+                </p>
+
+            </aside>
+
+            <main>
+
+                <div className="main-content">
+
+                    <img src={logoImg} alt="Letmeask" />
+
+                    <button onClick={handleCreateRoom} className="create-room">
+
+                        <img src={googleIconImg} alt="Google Icon" />
+
+                        Create your room with google
+
+                    </button>
+
+                    <div className="separator">
+                        Or enter a room
+                    </div>
+
+                    <form onSubmit={handleJoinRoom}>
+
+                        <input 
+                        type="text" 
+                        placeholder="Enter your room code"
+                        onChange={event => setRoomCode(event.target.value)}
+                        value={roomCode}
+                        />
+
+                        <Button type="submit">
+                            Enter the room
+                        </Button>
+
+                    </form>
+
+                </div>
+
+            </main>
+
+        </div>
+    )
+}
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,88 @@
+import { FormEvent, useState } from 'react'
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+
+import { Button } from '../components/Button';
+import { database } from '../services/firebase';
+import { useAuth } from '../hooks/useAuth';
+
+import '../styles/auth.scss';
+
+export function NewRoom() {
+    const { user } = useAuth()
+    const navigate = useNavigate()
+    const [newRoom, setNewRoom] = useState('');
+
+    async function handleCreateRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (newRoom.trim() === '') {
+            return;
+        }
+
+        const roomRef = database.ref('rooms');
+
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id,
+        })
+
+        navigate(`/rooms/${firebaseRoom.key}`)
+    }
+
+    return (
+
+        <div id="page-auth">
+
+            <aside>
+
+                <img src={illustrationImg} alt="Illustration symbolizing questions and answers" />
+
+                <strong>
+                    Create live Q&amp;A rooms
+                </strong>
+
+                <p>
+                    Clear your audience's doubts in real time
+                </p>
+
+            </aside>
+
+            <main>
+
+                <div className="main-content">
+
+                    <img src={logoImg} alt="Letmeask" />
+
+                    <h2>
+                        Create a new room
+                    </h2>
+
+                    <form onSubmit={handleCreateRoom}>
+
+                        <input 
+                        type="text" 
+                        placeholder="Room name"
+                        onChange={event => setNewRoom(event.target.value)}
+                        value={newRoom}
+                        />
+
+                        <Button type="submit">
+                            Create room
+                        </Button>
+
+                    </form>
+
+                    <p>
+                        Want to join an existing room? <Link to="/"> Click here </Link> 
+                    </p>
+
+                </div>
+
+            </main>
+
+        </div>
+    )
+}
